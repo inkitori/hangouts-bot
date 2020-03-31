b42d70264c2917f2d9d7c542e8708079b6c3110f
Yellow <52297896+YellowPapaya@users.noreply.github.com>
2020-03-30 22:53:41 -0700

    eeeeeeeeeks

--- handler.py
+++ handler.py
@@ -1,10 +1,12 @@
 import hangups
+from hangups import hangouts_pb2 
+from hangups.hangouts_pb2 import ParticipantId
 import asyncio
 import random
 from text_2048 import play_game
 from collections import defaultdict
-
-
+from datetime import datetime, tzinfo
+from utils import *
 
 class Handler:
     def __init__(self):
@@ -13,24 +15,40 @@ class Handler:
                 "bad bot": "nuu dun pweese~~ >.<",
                 "headpat": "uwu thanku",
                 "yamete": "kudasai!~~",
-                "ping": "pong"
+                "ping": "pong",
+                "pong": "ping",
+                "saber": "hi"
         }
         self.commands = {
                 "/help": self.help,
                 "/rename": self.rename,
                 "/say": self.say,
                 "/rickroll": self.rickroll,
-                "/quit": self.quit
+                "/quit": self.quit,
+                "/reset": self.reset,
+                "/id": self.id,
+                "/kick": self.kick
         }
         self.images = {
                 "/gay": "images/gay.jpg",
                 "/math": "images/math.jpg",
                 "/praise": "images/praise.jpg",
                 "/goddammit": "images/goddammit.jpg",
-                #"/heymister": "images/heymister.png"
+                "/heymister": "images/heymister.png"
             }
         self.users = defaultdict(dict) 
-        self.admins = ["yelloweywatermelons@gmail.com", "mskikotang@gmail.com"] # fill in yourself
+        self.admins = [] # fill in yourself (store as int)
+
+    # utility
+    async def help(self, bot, event):
+        user, conv = getUserConv(bot, event)
+        if self.cooldown(user, event, 10):
+            return
+
+        f = open("help.txt", 'r')
+        contents = f.read()
+        await conv.send_message(toSeg(contents))
+        f.close()
 
     async def rename(self, bot, event):
         user, conv = getUserConv(bot, event)
@@ -40,7 +58,7 @@ class Handler:
         try:
             await conv.rename(event.text.split(' ', 1)[1])
         except:
-            await conv.send_message(hangups.ChatMessageSegment.from_str("Format: /rename {name}"))
+            await conv.send_message(toSeg("Format: /rename {name}"))
 
     async def say(self, bot, event):
         user, conv = getUserConv(bot, event)
@@ -48,60 +66,101 @@ class Handler:
             return
 
         try:
-            await conv.send_message(hangups.ChatMessageSegment.from_str(event.text.split(' ', 1)[1]))
+           await conv.send_message(toSeg(event.text.split(' ', 1)[1]))
         except:
-            await conv.send_message(hangups.ChatMessageSegment.from_str("Format: /say {message}"))
-
-    async def rickroll(self, bot, event):
-        user, conv = getUserConv(bot, event)
-        if self.cooldown(user, event, 2):
-            return
-
-        try:
-            await conv.send_message(hangups.ChatMessageSegment.from_str("https://youtu.be/dQw4w9WgXcQ"))
-        except:
-            await conv.send_message(hangups.ChatMessageSegment.from_str("Something went wrong!"))
-
-    async def quit(self, bot, event):
-        user, conv = getUserConv(bot, event)
-
-        try:
-            if str(user.emails).strip().lower()[2:-2] in self.admins:
-                await conv.send_message(hangups.ChatMessageSegment.from_str("Saber out!"))
-                await bot.client.disconnect()
-            else:
-                await conv.send_message(hangups.ChatMessageSegment.from_str("bro wtf u can't use that"))
-        except:
-                await conv.send_message(hangups.ChatMessageSegment.from_str("Something went wrong!"))
-
-    async def help(self, bot, event):
+            await conv.send_message(toSeg("Format: /say {message}"))
+    async def id(self, bot, event):
         user, conv = getUserConv(bot, event)
         if self.cooldown(user, event, 10):
             return
 
-        f = open("help.txt", 'r')
-        contents = f.read()
-        await conv.send_message(hangups.ChatMessageSegment.from_str(contents))
-        f.close()
+        try:
+            await conv.send_message(toSeg(user.id_[0]))
+        except:
+            await conv.send_message(toSeg(str("Something went wrong!")))
 
+    async def kick(self, bot, event):
+        user, conv = getUserConv(bot, event)
+        arg1 = event.text.lower().split()[1]
+        users = conv.users
+        ids = []
+        kick_users = []
+
+        #try:
+        for user in users:
+            if arg1 in user.full_name.lower():
+                kick_users.append(user)
+
+        if not kick_users:
+            await conv.send_message(toSeg("Nobody in this conversation goes by that name"))
+            return
+        # only reason i figured this out was because of hangupsbot, so thank you so much https://github.com/xmikos/hangupsbot/blob/master/hangupsbot/commands/conversations.py
+
+        ids = [ParticipantId(gaia_id = user.id_.gaia_id, chat_id = conv.id_) for user in kick_users]
+
+        for kick_id in ids:
+            request = hangouts_pb2.RemoveUserRequest(request_header = bot.client.get_request_header(), participant_id = kick_id, event_request_header = conv._get_event_request_header())
+            res = await bot.client.remove_user(request)
+            conv.add_event(res.created_event)
+        #except:
+            #await conv.send_message(toSeg("Yeah don't use this command lol"))
+
+    # fun
+    async def rickroll(self, bot, event):
+        user, conv = getUserConv(bot, event)
+        if self.cooldown(user, event, 3):
+            return
+
+        try:
+            await conv.send_message(toSeg("https://youtu.be/dQw4w9WgXcQ"))
+        except:
+            await conv.send_message(toSeg("Something went wrong!"))
+
+    # config 
+    async def quit(self, bot, event):
+        user, conv = getUserConv(bot, event)
+        if self.cooldown(user, event, 30):
+            return
+
+        try:
+            if self.isAdmin(user):
+                await conv.send_message(toSeg("Saber out!"))
+                await bot.client.disconnect()
+            else:
+                await conv.send_message(toSeg("bro wtf u can't use that"))
+        except:
+                await conv.send_message(toSeg("Something went wrong!"))
+
+    async def reset(self, bot, event):
+        user, conv = getUserConv(bot, event)
+
+        try:
+            arg1 = '/' + event.text.lower().split()[1]
+            if self.isAdmin(user):
+                if arg1 in self.users[user]:
+                    self.users[user][arg1] = datetime.min.replace(tzinfo=None)
+                else:
+                    await conv.send_message(toSeg("Format: /reset {command}"))
+            else:
+                await conv.send_message(toSeg("bro wtf u can't use that"))
+        except:
+            await conv.send_message(toSeg("Format: /reset {command}"))
+
+    # helpers
     def cooldown(self, user, event, cooldown):
         text = event.text.lower()
+        strippedTime = event.timestamp.replace(tzinfo=None)
 
         if user in self.users and text.split()[0] in self.users[user]:
-            if (event.timestamp - self.users[user][text.split()[0]]).seconds < cooldown:
-                print(cooldown - (event.timestamp - self.users[user][text.split()[0]]).seconds)
+            if (strippedTime - self.users[user][text.split()[0]]).seconds < cooldown:
+                print(cooldown - (strippedTime - self.users[user][text.split()[0]]).seconds)
                 return 1
             else:
-                self.users[user][text.split()[0]] = event.timestamp
-                return 0
+                self.users[user][text.split()[0]] = strippedTime 
         else:
-            self.users[user][text.split()[0]] = event.timestamp
-            print("bruh")
-            return 0
+            self.users[user][text.split()[0]] = strippedTime
 
-def getUserConv(bot, event):
-    conv = bot._convo_list.get(event.conversation_id)
-    user = conv.get_user(event.user_id)
-
-    return user, conv
-
+    def isAdmin(self, user):
+        print(type(user.id_[0]))
+        if int(user.id_[0]) in self.admins:
+            return True
