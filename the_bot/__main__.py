3c88f32163c184f258cd8677cd120d5579c91de2
Creonalia <chendi.luo@gmail.com>
2020-04-09 22:29:21 -0700

    deleted old bot selection code

--- the_bot/__main__.py
+++ the_bot/__main__.py
@@ -16,25 +16,6 @@ bot_option = optparse.make_option(
 parser.add_option(bot_option)
 
 
-def choose_bot_input():
-    """
-    lets the user manually choose a bot
-    replaced by command line arguments
-    """
-    bot_selection_text = "Pick a bot from the list below or use quit\n"
-    bot_selection_text += utils.newline(utils.join_items(
-        *[(name, bot.__doc__) for name, bot in bots.items()], is_description=True
-    ))
-    command = ""
-    while command not in bots.keys():
-        commands = utils.command_parser(input(bot_selection_text))
-        command = next(commands)
-        if command == "quit":
-            sys.exit()
-
-    return bots[command]()
-
-
 def choose_bot_arguments():
     (options, args) = parser.parse_args()
     optparse.check_choice(bot_option, "bot", options.bot)
