a325c37955ea5922c535205e0a23c09c8814ae1e
Chendi Luo <chendi.luo@gmail.com>
2020-05-02 10:57:36 -0700

    bug in inventory

--- the_bot/rpg/classes.py
+++ the_bot/rpg/classes.py
@@ -278,7 +278,7 @@ class Item:
         health=0, attack=0, defense=0, mana=0, level=1,
         description="what, you thought we would write flavor text for everything? pft"
     ):
-        self.name = name
+        self.name = name.lower()
         self.type_ = ItemType(type_.lower())
         self.rarity = Rarity(int(rarity))
         self.modifier = modifier
